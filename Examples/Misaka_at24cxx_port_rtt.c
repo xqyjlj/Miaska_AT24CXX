aa76ee6ee7e7be8bee0bc03890923af5ed59d35b
xqyjlj <xqyjlj@126.com>
2021-09-07 13:33:52 +0800

    添加文件信息说明

--- Examples/Misaka_at24cxx_port_rtt.c
+++ Examples/Misaka_at24cxx_port_rtt.c
@@ -1,3 +1,19 @@
+/**
+ * @file Misaka_at24cxx_port_rtt.c
+ * @brief
+ * @author xqyjlj (xqyjlj@126.com)
+ * @version 0.0
+ * @date 2021-09-07
+ * @copyright Copyright © 2020-2021 xqyjlj<xqyjlj@126.com>
+ *
+ * *********************************************************************************
+ * @par ChangeLog:
+ * <table>
+ * <tr><th>Date       <th>Version <th>Author  <th>Description
+ * <tr><td>2021-09-07 <td>0.0     <td>xqyjlj  <td>内容
+ * </table>
+ * *********************************************************************************
+ */
 #include "misaka_at24cxx.h"
 #include "rtthread.h"
 #include "rtdevice.h"
--- Misaka_at24cxx.h
+++ Misaka_at24cxx.h
@@ -1,11 +1,18 @@
-/*
- * Copyright (c) 2020-2020,  by xqyjlj
+/**
+ * @file Misaka_at24cxx.h
+ * @brief
+ * @author xqyjlj (xqyjlj@126.com)
+ * @version 0.0
+ * @date 2021-09-07
+ * @copyright Copyright © 2020-2021 xqyjlj<xqyjlj@126.com>
  *
- * SPDX-License-Identifier: Apache License 2.0
- *
- * Change Logs:
- * Date           Author       Notes                    Email
- * 2021-05-13     xqyjlj       the first version        xqyjlj@126.com
+ * *********************************************************************************
+ * @par ChangeLog:
+ * <table>
+ * <tr><th>Date       <th>Version <th>Author  <th>Description
+ * <tr><td>2021-09-07 <td>0.0     <td>xqyjlj  <td>内容
+ * </table>
+ * *********************************************************************************
  */
 
 #ifndef __MISAKA_AT24CXX_H__
